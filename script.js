33a959eb30935cebec0759c08534883c850e33db
Kiran Panesar <KiranPanesar@users.noreply.github.com>
2017-05-10 16:00:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ window.addEventListener('input', function (e) {
  var buttonTitleField = document.getElementById("buttontitle");
 
  var cardLink = document.getElementById("cardlink");
- cardlink.value = "%["+buttonTitleField.value+"](pairade://card?id="+cardIDField.value+")"
+ cardlink.value = "%["+buttonTitleField.value+"](pairade://card/"+cardIDField.value+")"
 
 }, false);
